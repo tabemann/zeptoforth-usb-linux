dfe8df4a92f7db63086ad289eec5e76f713b502a
serialcomms <serialcomms@outlook.com>
2025-01-05 10:11:11 +0000

    Add files via upload

--- usb_2_cdc_buffers.fs
+++ usb_2_cdc_buffers.fs
@@ -7,10 +7,10 @@
 \ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 \ copies of the Software, and to permit persons to whom the Software is
 \ furnished to do so, subject to the following conditions:
-\ 
+\
 \ The above copyright notice and this permission notice shall be included in
 \ all copies or substantial portions of the Software.
-\ 
+\
 \ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 \ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 \ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,146 +19,157 @@
 \ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 \ SOFTWARE.
 
+\ ring buffer sizes must be power-of-two values - see below
+
+\  7 bit constant = 128 bytes
+\  8 bit constant = 256 bytes
+\  9 bit constant = 512 bytes
+\ 10 bit constant = 1024 bytes
+\ 11 bit constant = 2048 bytes
+\ 12 bit constant = 4096 bytes
+
+\ tx and rx buffer sizes can be different if required
+\ adjust values locally for required usage scenarios.
+
 compile-to-flash
 
-marker remove-usb-cdc-buffers
+begin-module usb-cdc-buffers
 
-  begin-module usb-cdc-buffers
+\ Constant transmit ring buffer size - must be power-of-two value
+9 bit constant tx-buffer-size
 
-  \ Constant for number of bytes to buffer
-  4096 constant tx-buffer-size
+\ Constant receive ring buffer size  - must be power-of-two value
 
-  \ Constant for number of bytes to buffer
-  4096 constant rx-buffer-size
+8 bit constant rx-buffer-size
 
-  \ RAM variable for rx buffer read-index
-  variable tx-read-index
-  
-  \ RAM variable for rx buffer read-index
-  variable rx-read-index
+\ RAM variable for rx buffer read-index
+variable tx-read-index
 
-  \ RAM variable for rx buffer write-index
-  variable tx-write-index
-  
-  \ RAM variable for rx buffer write-index
-  variable rx-write-index
-  
-  \ tx buffer size mask
-  tx-buffer-size 1 - constant tx-buffer-mask 
- 
-  \ rx buffer size mask
-  rx-buffer-size 1 - constant rx-buffer-mask
-    
-  \ tx buffer to Host
-  tx-buffer-size buffer: tx-buffer
+\ RAM variable for rx buffer read-index
+variable rx-read-index
 
-  \ rx buffer to Pico
-  rx-buffer-size buffer: rx-buffer
-    
-  : init-tx-ring ( -- )
-    0 tx-read-index !
-    0 tx-write-index !
-    tx-buffer tx-buffer-size 0 fill
-  ;
+\ RAM variable for rx buffer write-index
+variable tx-write-index
 
-  : init-rx-ring ( -- )
-    0 rx-read-index !
-    0 rx-write-index ! 
-    rx-buffer rx-buffer-size 0 fill
-  ;
+\ RAM variable for rx buffer write-index
+variable rx-write-index
 
-  : tx-size ( -- u )
-    tx-buffer-size 1 - 
-  ;
+\ tx buffer size mask
+tx-buffer-size 1 - constant tx-buffer-mask
 
-  : rx-size ( -- u )
-    rx-buffer-size 1 -
-  ;
+\ rx buffer size mask
+rx-buffer-size 1 - constant rx-buffer-mask
 
-  : tx-used ( -- u )
-    tx-read-index @ { read-index }
-    tx-write-index @ { write-index }
-    read-index write-index <= if
-      write-index read-index -
-    else
-      tx-buffer-size read-index - write-index +
-    then
-  ;
+\ tx buffer to Host
+tx-buffer-size buffer: tx-buffer
 
-  : rx-used ( -- u )   
-    rx-read-index @ { read-index }
-    rx-write-index @ { write-index }
-    read-index write-index <= if
-      write-index read-index -
-    else
-      rx-buffer-size read-index - write-index +
-    then  
-  ;
-    
-  \ Get number of free bytes available in tx buffer
-  : tx-free ( -- bytes ) 
-    tx-buffer-size 1 - tx-used - 
-  ;
+\ rx buffer to Pico
+rx-buffer-size buffer: rx-buffer
 
-  \ Get number of free bytes available in rx buffer
-  : rx-free ( -- bytes )   
-    rx-buffer-size 1 - rx-used -  
-  ;
+: init-tx-ring ( -- )
+  0 tx-read-index !
+  0 tx-write-index !
+  tx-buffer tx-buffer-size 0 fill
+;
 
-  \ Get whether the tx buffer is full
-  : tx-full? ( -- f )
-    tx-write-index @ 1 + tx-buffer-mask and tx-read-index @ =
-  ;
-  
-  \ Get whether the rx buffer is full
-  : rx-full? ( -- f )    
-    rx-write-index @ 1 + rx-buffer-mask and rx-read-index @ =
-  ;
+: init-rx-ring ( -- )
+  0 rx-read-index !
+  0 rx-write-index !
+  rx-buffer rx-buffer-size 0 fill
+;
 
-   \ Get whether the tx buffer is empty
-  : tx-empty? ( -- f )
-    tx-write-index @ tx-read-index @ = 
-  ;
+: tx-size ( -- u )
+  tx-buffer-size 1 -
+;
 
-  \ Get whether the rx buffer is empty
-  : rx-empty? ( -- f )  
-    rx-write-index @ rx-read-index @ = 
-  ;
+: rx-size ( -- u )
+  rx-buffer-size 1 -
+;
 
-  \ USB read byte from tx ring buffer 
-  : read-tx ( -- c )  
-    tx-empty? if 0 else
-    tx-read-index @ tx-buffer + c@     
-    tx-read-index @ 1 + tx-buffer-mask and tx-read-index !  
-    then
-  ;
+: tx-used ( -- u )
+  tx-read-index @ { read-index }
+  tx-write-index @ { write-index }
+  read-index write-index <= if
+   write-index read-index -
+  else
+   tx-buffer-size read-index - write-index +
+  then
+;
 
-  \ Console read byte from rx ring buffer
-  : read-rx ( -- c )
-    rx-empty? if 0 else
-    rx-read-index @ rx-buffer + c@
-    rx-read-index @ 1 + rx-buffer-mask and rx-read-index !
-    then
-  ;
+: rx-used ( -- u )
+  rx-read-index @ { read-index }
+  rx-write-index @ { write-index }
+  read-index write-index <= if
+   write-index read-index -
+  else
+   rx-buffer-size read-index - write-index +
+  then
+;
 
-  \ Console write byte to tx ring buffer
-  : write-tx ( c -- ) 
-    tx-full? if drop else
-      tx-write-index @ tx-buffer + c!
-      tx-write-index @ 1 + tx-buffer-mask and tx-write-index !
-    then
-  ;
+\ Number of free bytes available in tx buffer
+: tx-free ( -- bytes )
+  tx-buffer-size 1 - tx-used -
+;
 
-  \ USB write byte to rx ring buffer
-  : write-rx ( c -- )
-    rx-full? if drop else
-      rx-write-index @ rx-buffer + c!
-      rx-write-index @ 1 + rx-buffer-mask and rx-write-index !
-    then
-  ;
+\ Number of free bytes available in rx buffer
+: rx-free ( -- bytes )
+  rx-buffer-size 1 - rx-used -
+;
 
-  end-module
+\ Transmit ring buffer full flag
+: tx-full? ( -- f )
+  tx-write-index @ 1 + tx-buffer-mask and tx-read-index @ =
+;
 
-  compile-to-ram
+\ Receive ring buffer full flag
+: rx-full? ( -- f )
+  rx-write-index @ 1 + rx-buffer-mask and rx-read-index @ =
+;
 
-  \ USB_2_CDC_BUFFERS (NEW) END =============================================== 
+\ Transmit ring buffer empty flag
+: tx-empty? ( -- f )
+  tx-write-index @ tx-read-index @ =
+;
+
+\ Receive ring buffer empty flag
+: rx-empty? ( -- f )
+  rx-write-index @ rx-read-index @ =
+;
+
+\ USB read byte from tx ring buffer
+: read-tx ( -- c )
+  tx-empty? if 0 else
+  tx-read-index @ tx-buffer + c@
+  tx-read-index @ 1 + tx-buffer-mask and tx-read-index !
+  then
+;
+
+\ Console read byte from rx ring buffer
+: read-rx ( -- c )
+  rx-empty? if 0 else
+  rx-read-index @ rx-buffer + c@
+  rx-read-index @ 1 + rx-buffer-mask and rx-read-index !
+  then
+;
+
+\ Console write byte to tx ring buffer
+: write-tx ( c -- )
+  tx-full? if drop else
+  tx-write-index @ tx-buffer + c!
+  tx-write-index @ 1 + tx-buffer-mask and tx-write-index !
+  then
+;
+
+\ USB write byte to rx ring buffer
+: write-rx ( c -- )
+  rx-full? if drop else
+  rx-write-index @ rx-buffer + c!
+  rx-write-index @ 1 + rx-buffer-mask and rx-write-index !
+  then
+;
+
+end-module
+
+compile-to-ram
+
+\ USB_2_CDC_BUFFERS (JANUARY 2025) END ===============================================
--- usb_4_console_minimal.fs
+++ usb_4_console_minimal.fs
@@ -7,10 +7,10 @@
 \ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 \ copies of the Software, and to permit persons to whom the Software is
 \ furnished to do so, subject to the following conditions:
-\ 
+\
 \ The above copyright notice and this permission notice shall be included in
 \ all copies or substantial portions of the Software.
-\ 
+\
 \ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 \ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 \ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,157 +19,197 @@
 \ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 \ SOFTWARE.
 
-compile-to-flash
+\ IMPORTANT - many terminal clients use the same control keys as zeptoforth.
+\ use set-usb-control-keys-off to allow use of more terminal clients.
+\ Note that some clients may hang if Pico reboots and does not recover.
 
-marker remove-usb-console
+compile-to-flash
 
 begin-module usb-console
 
-  console import
-  
-  begin-module usb-internal
+console import
 
-  task import
-  usb-core import
-  usb-constants import
-  usb-cdc-buffers import
+begin-module usb-console-internal
 
-  : start-ep1-ring-transfer-to-host ( -- )
-    tx-empty? not if
-    tx-used 64 min { tx-bytes }         
-    tx-bytes 0 do 
-      read-tx EP1-to-Host dpram-address @ i + c! 
-    loop
+task import
+usb-core import
+usb-constants import
+usb-cdc-buffers import
 
-    EP1-to-Host tx-bytes usb-send-data-packet
+variable usb-control-keys-enabled?
 
+: console-control-c-handler ( -- )
+
+  usb-control-keys-enabled? @ if
+
+   [: cr ." Control-C pressed - rebooting" cr ;] usb-core-debug
+
+   usb-set-modem-offline 
+   20 ms
+   usb-remove-device
+   20 ms
+   reboot
+
+  else
+   \ ignore control-c and carry on receiving data
+   false EP1-to-Pico endpoint-busy? !
+  then
+;
+
+: console-control-t-handler ( -- )
+
+  usb-control-keys-enabled? @ if
+
+   [: cr ." Control-T pressed- attention" cr ;] usb-core-debug
+
+   attention? @ if
+     [: attention-hook @ execute ;] try    
+     ?raise
+   else
+     [: attention-start-hook @ execute ;] try  \ not working - ask Travis
+     ?raise
     then
-  ;
+   else
+  then
+  \ continue data reception
+  false EP1-to-Pico endpoint-busy? !
+;
 
-  : start-ep1-data-transfer-to-pico ( -- )
-    rx-free 63 > if
-      EP1-to-Pico 64 usb-receive-data-packet
-    then 
-  ;
-     
-  \ USB Start of Frame Interrupts, every 1 ms
-  : handle-sof-from-host ( -- )
+: start-ep1-data-transfer-to-host ( -- )
+  tx-empty? not if
+   ep1-start-ring-transfer-to-host
+  then
+;
 
-    EP1-to-Pico busy? @ not if start-ep1-data-transfer-to-pico then
-    EP1-to-Host busy? @ not if start-ep1-ring-transfer-to-host then 
-  ;
+: start-ep1-data-transfer-to-pico ( -- )
+  rx-free 63 > if
+   EP1-to-Pico 64 usb-receive-data-packet
+  then
+;
 
-    \ Byte available to read from rx ring buffer ?
-  : usb-key? ( -- key? )   
-    rx-empty? not 
-  ;
+\ USB Start of Frame Interrupts, every 1 ms
+: handle-sof-from-host ( -- )
+  EP1-to-Pico endpoint-busy? @ not if start-ep1-data-transfer-to-pico then
+  EP1-to-Host endpoint-busy? @ not if start-ep1-data-transfer-to-host then
+;
 
-    \ Client connected and tx ring buffer capacity available ?
-  : usb-emit? ( -- emit? )
-    usb-dtr? tx-full? not and
-  ;
+\ Byte available to read from rx ring buffer ?
+: usb-key? ( -- key? )
+  rx-empty? not
+;
 
-  : usb-emit ( c -- )
-    tx-full? if
-      begin pause-reschedule-last tx-full? not until
-    then
-    write-tx
-  ;
+\ Client connected and tx ring buffer capacity to host available ?
+: usb-emit? ( -- emit? )
+   usb-dtr? tx-full? not and
+;
 
-  : usb-key ( -- c)
-    begin
-      rx-empty? if
-        pause-reschedule-last false
-      else
-        read-rx true    
-      then
-    until  
-  ;
+: usb-emit ( c -- )
+  tx-full? if
+   begin pause-reschedule-last tx-full? not until
+  then
+  write-tx
+;
 
-  : usb-flush-console ( -- )
-    begin pause-reschedule-last tx-empty? until
-  ;
+: usb-key ( -- c)
+  begin
+  rx-empty? if
+   pause-reschedule-last false
+  else
+   read-rx true
+  then
+  until
+;
 
-  \ Switch to USB console
-  : switch-to-usb-console
+: usb-flush-console ( -- )
+  begin pause-reschedule-last tx-empty? until
+;
 
-    ['] usb-key? key?-hook !
-    ['] usb-key key-hook !
-    ['] usb-emit? emit?-hook !
-    ['] usb-emit emit-hook !
+\ Switch to USB console
+: switch-to-usb-console
 
-    ['] usb-emit? error-emit?-hook !
-    ['] usb-emit error-emit-hook !
-    ['] usb-flush-console flush-console-hook !
-    ['] usb-flush-console error-flush-console-hook !
-  ;
+  ['] usb-key? key?-hook !
+  ['] usb-key key-hook !
+  ['] usb-emit? emit?-hook !
+  ['] usb-emit emit-hook !
 
-   \ Set the curent input to usb within an xt
-  : with-usb-input ( xt -- )
-    ['] usb-key ['] usb-key? rot with-input
-  ;
+  ['] usb-emit? error-emit?-hook !
+  ['] usb-emit error-emit-hook !
+  ['] usb-flush-console flush-console-hook !
+  ['] usb-flush-console error-flush-console-hook !
+;
 
-  \ Set the current output to usb within an xt
-  : with-usb-output ( xt -- )
-    ['] usb-emit ['] usb-emit? rot ['] usb-flush-console swap with-output
-  ;
+\ Set the curent input to usb within an xt
+: with-usb-input ( xt -- )
+  ['] usb-key ['] usb-key? rot with-input
+;
 
-  \ Set the current error output to usb within an xt
-  : with-usb-error-output ( xt -- )
-    ['] usb-emit ['] usb-emit? rot ['] usb-flush-console swap with-error-output
-  ;
+\ Set the current output to usb within an xt
+: with-usb-output ( xt -- )
+  ['] usb-emit ['] usb-emit? rot ['] usb-flush-console swap with-output
+;
 
-  : usb-wait-for-device-configured ( -- )
+\ Set the current error output to usb within an xt
+: with-usb-error-output ( xt -- )
+  ['] usb-emit ['] usb-emit? rot ['] usb-flush-console swap with-error-output
+;
 
-    cr ." Waiting for Host to set USB Device Configuration ... " cr
+: usb-wait-for-device-configured ( -- )
 
-    begin pause-reschedule-last usb-device-configured? @ not until
-    
-    cr
+  cr ." Waiting for Host to set USB Device Configuration ... " cr
 
-    ." --------------------------------------------------------- " cr
-    ." USB Device Configured, Starting SOF callback handler now " cr
+  begin pause-reschedule-last usb-device-configured? @ not until
 
-    ['] handle-sof-from-host sof-callback-handler !
-  ;
+  cr
 
-  : usb-wait-for-client-connect ( -- )
+  ." --------------------------------------------------------- " cr
+  ." USB Device Configured, Starting SOF callback handler now " cr
 
-    cr ." Waiting for Client to Connect (DTR signal from Host) ... " cr
+  ['] handle-sof-from-host sof-callback-handler !
+  ['] console-control-c-handler ep1-control-c-handler !
+  ['] console-control-t-handler ep1-control-t-handler !
+;
 
-    begin pause-reschedule-last usb-dtr? until
- 
-    cr
+: usb-wait-for-client-connect ( -- )
 
-    ." --------------------------------------------------------- " cr
-    ." Client Connected, starting USB CDC/ACM Serial Console now"  cr
+  cr ." Waiting for Client to Connect (DTR signal from Host) ... " cr
 
-  ;
+  begin pause-reschedule-last usb-dtr? until
 
-  \ Initialize USB console
-  : init-usb-console ( -- )
+  cr
 
-    init-usb
-    init-tx-ring
-    init-rx-ring
+  ." --------------------------------------------------------- " cr
+  ." Client Connected, starting USB CDC/ACM Serial Console now"  cr
 
-    usb-insert-device    
-    usb-wait-for-device-configured
-    usb-wait-for-client-connect
-    usb-set-modem-online
-    switch-to-usb-console
-    welcome-usb
-  ;
+;
 
-  initializer init-usb-console
-    
-  end-module> import
+\ Initialize USB console
+: init-usb-console ( -- )
 
-  \ Select the USB console
-  : usb-console ( -- ) switch-to-usb-console ;
+  init-usb
+  init-tx-ring
+  init-rx-ring
+
+  usb-insert-device
+  usb-wait-for-device-configured
+  usb-wait-for-client-connect
+  usb-set-modem-online
+  switch-to-usb-console
+  true usb-control-keys-enabled? !
+;
+
+initializer init-usb-console
+
+end-module> import
+
+\ Select the USB serial console
+: usb-console ( -- ) switch-to-usb-console ;
+: set-usb-control-keys-on ( -- ) true usb-control-keys-enabled? ! ;
+: set-usb-control-keys-off ( -- ) false usb-control-keys-enabled? ! ;
 
 end-module
 
+\ : turnkey cr ." USB Serial Console Turnkey Test " cr ; \ not working here - ask Travis
+
 compile-to-ram
 
-\ USB_4_CDC_CONSOLE V4 - LINUX COMPATIBILITY END =============================================== 
+\ USB_4_CDC_CONSOLE (JANUARY 2025) END ===============================================
